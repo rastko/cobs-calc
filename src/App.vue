af27f9b29122b6b91bac5d8855c195f09eca57f7
rastko <ludi.rashid@gmail.com>
2020-04-16 20:21:54 +0200

    Add PUG support

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
-<template>
-  <div id="app">
-    <div class="container">
-      <div class="header" style="margin: 40px 0 20px 0">
-        <div class="row">
-          <div class="form-group">
-            <input type="number" v-model="sum" class="form-control" placeholder="Amount" />
-          </div>
-        </div>
-      </div>
+<template lang="pug">
+#app
+  .container
+    .header(style="margin: 40px 0 20px 0")
+      .row
+        .form-group
+          input.form-control(type="number", v-model="sum", placeholder="Amount")
 
-      <div class="row">
-        <table class="table table-striped">
-          <tr>
-            <th>Shares</th>
-            <th>Receives</th>
-          </tr>
-          <tr v-for="index in 9" :key="index">
-            <td>{{ index }}</td>
-            <td>{{ sum / 10 * index }}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-  </div>
+      .row
+        table.table.table-striped
+          tr
+            th Shares
+            th Receives
+          tr(v-for="index in 9", :key="index")
+            td {{ index }}
+            td {{ sum / 10 * index }}
 </template>
 
 <script>
@@ -30,8 +21,8 @@ export default {
   name: "app",
   data: () => {
     return {
-      sum: 0
+      sum: 0,
     };
-  }
+  },
 };
 </script>
