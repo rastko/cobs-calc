2d052f85dc3a98ab5bc70604da46a69a9cb6fbc5
rastko <ludi.rashid@gmail.com>
2020-04-18 15:29:38 +0200

    Add components folder

---
+++ src/components/results.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  tr
+    td {{ index }}
+    td {{ value }}
+</template>
+
+<script>
+export default {
+  name: "results",
+  props: ["index", "value"]
+};
+</script>
---
+++ src/components/user-input.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+  .row
+    form
+      .form-group
+        input.form-control(type="number" @input="$emit('input', $event.target.value)")
+</template>
+
+<script>
+export default {
+  name: "user-input"
+};
+</script>
